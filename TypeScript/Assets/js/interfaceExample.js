f40f322590d4e48f65a2c7da52d3a8b8ee7b0cb7
PriyankaBawanna <81788297+PriyankaBawanna@users.noreply.github.com>
2021-11-07 22:43:26 +0530

    Add files via upload

---
+++ TypeScript/Assets/js/interfaceExample.js
@@ -0,0 +1,6 @@
+var OperatingSystem = function (type) {
+  console.log("Android " + type.name + " has " + type.language + " language.");
+};
+var Oreo = { name: "O.S", language: "Java" };
+OperatingSystem(Oreo);
+console.log("this is java  script interface");
